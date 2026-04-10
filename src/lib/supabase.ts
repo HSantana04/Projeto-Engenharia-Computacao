d878f9b755aaac48d48ec86d47c1c7834df0bc2e
HSantana04 <131053627+HSantana04@users.noreply.github.com>
2026-04-10 17:39:14 -0300

    .
    Updated Supabase URL and Anon Key directly in the code.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../types/database.types';
+import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://jtfwavoevjxzwjpegktw.supabase.co";
+
+
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0Zndhdm9ldmp4endqcGVna3R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNDg3NzQsImV4cCI6MjA4OTkyNDc3NH0.eXG-qMOXFTtlV3Xn-_RcDTUbkNp3GtmfaDrpcHXnSw4";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
